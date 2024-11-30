d1bddc156d2353f805714539c1009d2ff279dd5c
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-01 03:37:05 +0530

    completed auth route

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,3 +52,53 @@ module.exports.loginUser = async (req, res) => {
     console.log(error);
   }
 };
+
+module.exports.getUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return res.status(400).json({ message: "Cannot find user" });
+    }
+
+    res.status(200).json({ message: "User fetched succesfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.updateUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { name, membershipType } = req.body;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({ message: "Cannot find user" });
+    }
+
+    user.name = name || user.name;
+    user.membershipType = membershipType || user.membershipType;
+    await user.save();
+
+    res.status(200).json({ message: "User saved" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.deleteUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) {
+      return res.status(400).json({ message: "Cannot find user" });
+    }
+
+    res.status(200).json({ message: "User deleted succesfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
-const { registerUser, loginUser } = require("../controllers/authController");
+const auth = require("../middlewares/auth");
+const {
+  registerUser,
+  loginUser,
+  getUser,
+  updateUser,
+} = require("../controllers/authController");
 
 const router = require("express").Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/", auth, getUser);
+router.put("/", auth, updateUser);
 
 module.exports = router;
